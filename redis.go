58a16179ae2317a02847c1b4d11fb2573b67479c
max.tsai <adimax2953@gmail.com>
2025-03-26 20:32:04 +0800

    更新包

--- redis.go
+++ redis.go
@@ -2,6 +2,7 @@ package goredis
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -14,15 +15,26 @@ type Option struct {
 	DB               int
 	PoolSize         int
 	ScriptDefinition string
+
+	PoolFIFO        bool
+	MaxIdleConns    int
+	ConnMaxIdleTime time.Duration
+	PoolTimeout     time.Duration
+	MinIdleConns    int
 }
 
 // Create - create a new redis descriptor
 func (opt *Option) Create() *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:        opt.Host + ":" + strconv.Itoa(opt.Port),
-		Password:    opt.Password,
-		DB:          opt.DB,
-		PoolSize:    opt.PoolSize,
-		ReadTimeout: -1,
+		Addr:            opt.Host + ":" + strconv.Itoa(opt.Port),
+		Password:        opt.Password,
+		DB:              opt.DB,
+		PoolSize:        opt.PoolSize,
+		ReadTimeout:     -1,
+		PoolFIFO:        opt.PoolFIFO,
+		MaxIdleConns:    opt.MaxIdleConns,
+		ConnMaxIdleTime: opt.ConnMaxIdleTime,
+		PoolTimeout:     opt.PoolTimeout,
+		MinIdleConns:    opt.MinIdleConns,
 	})
 }
