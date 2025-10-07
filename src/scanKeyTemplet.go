2d12eb9b379d9e062d1ee91f9eb14b8730de9135
adimax2953 <adimax2953@gmail.com>
2025-10-07 17:15:52 +0800

    Update scanKeyTemplet.go

--- src/scanKeyTemplet.go
+++ src/scanKeyTemplet.go
@@ -36,33 +36,32 @@ const (
 		Description :   ScanKey
 		EVALSHA  <script_sha1> 0 {DBKey} {Count} 
 		--]]
-		local DBKey                                         = tonumber(KEYS[1])
-		local Count                                    		= KEYS[2]
-		local sender                                        = "ScanKey.lua"
+		local DBKey = tonumber(KEYS[1])
+		local Count = tonumber(KEYS[2])
+		local sender = "ScanKey.lua"
 
-		local values = {}	
-
+		local values = {}
 
 		---@param cursor string
 		---@return string[]
 		local function scan_keys(cursor)
-			local result = redis.call('scan', cursor)
-			cursor = tonumber(result[1])
+			local result = redis.call('SCAN', cursor, 'COUNT', Count)
+			local next_cursor = result[1]
 			local keys = result[2]
 
 			for _, key in ipairs(keys) do
 				table.insert(values, key)
 			end
 
-			if cursor == 0 then
-				return keys
+			if next_cursor == "0" then
+				return
 			else
-				return scan_keys(cursor)
+				scan_keys(next_cursor)
 			end
 		end
-		
-		redis.call('select',DBKey)	
-		scan_keys(Count)
+
+		redis.call('SELECT', DBKey)
+		scan_keys("0")
 		return values
     `
 )
